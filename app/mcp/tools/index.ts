0992758718fa4426798f90b9f161df2c2fad9400
Gatsby Fitzgerald <gatsbytdfitzgerald@gmail.com>
2026-07-13 17:23:09 +1000

    71

--- app/mcp/tools/index.ts
+++
@@ -1,9 +0,0 @@
-import { registerEchoTool } from "./echo";
-import { registerSearchDocsTool } from "./search-docs";
-import { registerGetTimeApp } from "../mcpApps/get-time-app";
-
-export function registerAllTools(server: any): void {
-  registerEchoTool(server);
-  registerSearchDocsTool(server);
-  registerGetTimeApp(server);
-}
